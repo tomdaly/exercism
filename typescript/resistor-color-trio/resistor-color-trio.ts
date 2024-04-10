a3c4fb33087ba73bd4cc21e15f10c2aef98bb7d0
tomdaly <tomdaly@users.noreply.github.com>
2024-04-10 15:56:27 +0100

    add ' ohms' suffix to pass first test

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,24 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export const decodedResistorValue = (codes: string[]): number => {
+  // const ohmExponent = COLORS.indexOf(codes[2])
+  const ohms = parseInt(codes.slice(0, 2).map((code) => COLORS.indexOf(code)).join(""));
+  return `${ohms} ohms`;
 }
+
+const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+
+const METRIC_PREFIX = [
+  'kilo',
+  'mega',
+  'giga'
+]
