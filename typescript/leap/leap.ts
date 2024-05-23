0a604e9f4d8fefde6f64a620a20492c8118d9240
tomdaly <tomdaly@users.noreply.github.com>
2024-05-23 13:47:50 +0100

    add leap

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,9 @@
+export function isLeap(year: number): boolean {
+  const divisibleByFour = year % 4 == 0;
+  if (divisibleByFour) {
+    const divisibleByOneHundred = year % 100 == 0;
+    const divisibleByFourHundred = year % 400 == 0;
+    return divisibleByOneHundred ? divisibleByFourHundred : true;
+  }
+  return false
+}
